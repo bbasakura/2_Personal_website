7f239a7bc9ba6f99f84d0530c4b80ba62364ffe9
bbasakura <872860382@qq.com>
2019-12-20 17:33:19 +0800

    Site updated: 2019-12-20 17:33:18

---
+++ static/api/js/component/comm_tools.js
@@ -0,0 +1,3 @@
+// build time:Fri Dec 20 2019 17:33:13 GMT+0800 (GMT+08:00)
+!window._bd_share_is_recently_loaded&&window._bd_share_main.F.module("component/comm_tools",function(n,o){var t=function(){var n=window.location||document.location||{};return n.href||""},r=function(n,o){for(var t=n.length,r="",e=1;e<=o;e++){var a=Math.floor(t*Math.random());r+=n.charAt(a)}return r},e=function(){var n=(+new Date).toString(36),o=r("0123456789abcdefghijklmnopqrstuvwxyz",3);return n+o};o.getLinkId=e,o.getPageUrl=t});
+//rebuild by neat 
---
+++ static/api/js/trans/data.js
@@ -0,0 +1,3 @@
+// build time:Fri Dec 20 2019 17:33:13 GMT+0800 (GMT+08:00)
+
+//rebuild by neat 
