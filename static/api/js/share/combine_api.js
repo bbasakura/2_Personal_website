8b29e499964309ead65e4a37bb2470f9a7e46385
bbasakura <872860382@qq.com>
2019-12-25 11:53:14 +0800

    Site updated: 2019-12-25 11:53:13

--- static/api/js/share/combine_api.js
+++ static/api/js/share/combine_api.js
@@ -1,3 +1,3 @@
-// build time:Wed Dec 25 2019 10:41:30 GMT+0800 (GMT+08:00)
+// build time:Wed Dec 25 2019 11:18:27 GMT+0800 (GMT+08:00)
 window._bd_share_main.F.module("share/combine_api",function(n,a,i){var e=(n("base/tangram").T,n("base/class").Class),s=n("share/api_base");a.CombineApi=e.create(function(n){var a=this,i=null,e=null;a.setApi=function(n,a){i=n,e=a},a._init=function(){i&&e&&i.on("sharecompleted",function(n){})}},s.ApiBase)});
 //rebuild by neat 
--- static/api/js/share/likeshare.js
+++ static/api/js/share/likeshare.js
@@ -1,3 +1,3 @@
-// build time:Wed Dec 25 2019 10:41:30 GMT+0800 (GMT+08:00)
+// build time:Wed Dec 25 2019 11:18:27 GMT+0800 (GMT+08:00)
 
 //rebuild by neat 
